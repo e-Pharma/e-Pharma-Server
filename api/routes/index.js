0bf839cacf7c39315c6ec3dc2eaa7a6639d500cf
Sankha Jayalath <sankha.rc@gmail.com>
2020-05-04 11:00:39 +0530

    deployed on heroku

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,7 +8,8 @@ const authRoutes = require("./auth")
 router.get('/', function (req, res){
     console.log(__dirname)
     // res.sendFile($__dirname +'/homepage.html`)
-    res.sendFile(path.join(__dirname, '../../views', 'homepage.html'));
+    //res.sendFile(path.join(__dirname, '../../views', 'homepage.html'));
+    res.send("Welcome")
 });
 
 router.use('/auth',authRoutes)
