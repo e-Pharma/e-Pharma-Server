3c9a775bd97e1aec4d8420d1f0b3eda156cab6a1
BLasan <benuraab@gmail.com>
2020-07-04 10:30:17 +0530

    remove: .env variables used files

--- api/handlers/password_reset.js
+++ api/handlers/password_reset.js
@@ -1,13 +1,51 @@
-// const sgMail = require('@sendgrid/mail');
+// // const sgMail = require('@sendgrid/mail');
 
+// // const response = require("../utils/response");
+
+// // const Logger = require("../utils/logger");
+// // const logger = new Logger();
+
+// // exports.sendPasswordResetLink = async (req, res) => {
+// //     console.log(req.body)
+// //     sgMail.setApiKey("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");
+// //     const msg = {
+// //         to: req.body.to,
+// //         from: req.body.from,
+// //         subject: req.body.subject,
+// //         text: 'Reset Password',
+// //         html: req.body.message,
+// //     };
+
+// //     sgMail.send(msg, (err, result) => {
+// //         if(err) {
+// //             logger.error(err);
+// //             return response(res, null, 471, err);
+// //         } else {
+// //             logger.info("Success", result)
+// //             return response(res, null, 200, "Successfully Sent!");   
+// //         }
+// //     });
+
+// //     // sgMail.send(msg).then((messageID) => {
+// //     //     logger.info("Successfully Sent", messageID)
+// //     //     response(res, null);
+// //     // }).catch(err => {
+// //     //     logger.error(err)
+// //     //     response(res, null, 471, err)
+// //     // })
+// // }
+
+// var mailgun = require("mailgun-js");
 // const response = require("../utils/response");
-
+// require('dotenv').config()
+// var api_key = process.env.MAIL_GUN_API;
+// var DOMAIN = process.env.MAIL_GUN_DOMAIN;
+// var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 // const Logger = require("../utils/logger");
 // const logger = new Logger();
 
 // exports.sendPasswordResetLink = async (req, res) => {
 //     console.log(req.body)
-//     sgMail.setApiKey("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");
 //     const msg = {
 //         to: req.body.to,
 //         from: req.body.from,
@@ -16,54 +54,14 @@
 //         html: req.body.message,
 //     };
 
-//     sgMail.send(msg, (err, result) => {
+//     mailgun.messages().send(msg, function (err, body) {
 //         if(err) {
 //             logger.error(err);
 //             return response(res, null, 471, err);
 //         } else {
-//             logger.info("Success", result)
+//             logger.info("Success", body)
 //             return response(res, null, 200, "Successfully Sent!");   
 //         }
 //     });
 
-//     // sgMail.send(msg).then((messageID) => {
-//     //     logger.info("Successfully Sent", messageID)
-//     //     response(res, null);
-//     // }).catch(err => {
-//     //     logger.error(err)
-//     //     response(res, null, 471, err)
-//     // })
 // }
-
-var mailgun = require("mailgun-js");
-require('dotenv').config()
-var api_key = process.env.MAIL_GUN_API;
-var DOMAIN = process.env.MAIL_GUN_DOMAIN;
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
-const response = require("../utils/response");
-
-const Logger = require("../utils/logger");
-const logger = new Logger();
-
-exports.sendPasswordResetLink = async (req, res) => {
-    console.log(req.body)
-    const msg = {
-        to: req.body.to,
-        from: req.body.from,
-        subject: req.body.subject,
-        text: 'Reset Password',
-        html: req.body.message,
-        "o:tracking": 'False'
-    };
-
-    mailgun.messages().send(msg, function (err, body) {
-        if(err) {
-            logger.error(err);
-            return response(res, null, 471, err);
-        } else {
-            logger.info("Success", body)
-            return response(res, null, 200, "Successfully Sent!");   
-        }
-    });
-
-}
--- api/handlers/s3_bucket_upload.js
+++ api/handlers/s3_bucket_upload.js
@@ -1,32 +1,32 @@
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+// // Load the AWS SDK for Node.js
+// var AWS = require('aws-sdk');
 
-const Logger = require("../../api/utils/logger");
-const logger = new Logger();
+// const Logger = require("../../api/utils/logger");
+// const logger = new Logger();
 
-const response = require("../../api/utils/response");
+// const response = require("../../api/utils/response");
 
-const { export_params } = require('../config/s3_cofig');
-const s3 = export_params.s3;
-const uploadParams = export_params.params
+// const { export_params } = require('../config/s3_cofig');
+// const s3 = export_params.s3;
+// const uploadParams = export_params.params
 
-exports.uploadPrescriptionImage = async(req, res) => {
-    var file = req.files
-    console.log(file.file.name)
-    uploadParams.Body = file.file.data
-    uploadParams.Key = "Orders/" + file.file.name
-    console.log(uploadParams)
+// exports.uploadPrescriptionImage = async(req, res) => {
+//     var file = req.files
+//     console.log(file.file.name)
+//     uploadParams.Body = file.file.data
+//     uploadParams.Key = "Orders/" + file.file.name
+//     console.log(uploadParams)
 
-    s3.upload(uploadParams, function(err, data) {
-        if (err) {
-            console.log("Error", err);
-            // logger("Error Uploading!", err);
-            return response(res, err, 500, "Internal Server Error!")
-        }
-        if (data) {
-            console.log("Upload Success", data.Location);
-            return response(res, { url: data.Location }, 200, "Successfully Uploaded!");
-        }
-    });
+//     s3.upload(uploadParams, function(err, data) {
+//         if (err) {
+//             console.log("Error", err);
+//             // logger("Error Uploading!", err);
+//             return response(res, err, 500, "Internal Server Error!")
+//         }
+//         if (data) {
+//             console.log("Upload Success", data.Location);
+//             return response(res, { url: data.Location }, 200, "Successfully Uploaded!");
+//         }
+//     });
 
-}
+// }
