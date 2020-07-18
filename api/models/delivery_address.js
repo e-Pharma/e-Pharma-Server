3b0aca1a50de3bc2d518844779d8123c853811e2
ThiliniUdari <thilinihk96@gmail.com>
2020-07-18 13:19:53 +0530

    delivery address structure modified

--- api/models/delivery_address.js
+++ api/models/delivery_address.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 
 const deliveryAddressSchema = mongoose.Schema({
+ 
     _id:mongoose.Schema.Types.ObjectId,
-    clientId:{type:String,required:true},
-    type:{type:String,required:true},
-    city:{type:String,required:true},
-    address:{type:String,required:true}
-    // addresss:[{
-    //     type:{type:String,required:true},
-    //     city:{type:String,required:true},
-    //     address:[{type:String,required:true}]
-    // }]
-   
+    clientId:{type:String},
+    items:[{
+        id:mongoose.Schema.Types.ObjectId,
+        type:{type:String},
+        city:{type:String},
+        address:{type:String,required:true}
+    }]
 });
 
 module.exports =mongoose.model("deliveryAddress",deliveryAddressSchema);
