5262fba1dea04655864cf1472d226588a8ee52c2
NipuniYR <nipuniyr@gmail.com>
2020-10-08 01:30:15 +0530

    Admin medicine

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -7,6 +7,7 @@ const adminLogin = require("../handlers/admin_auth");
 const driverHandler = require('../handlers/admin_driver');
 const dashboardHandler =  require('../handlers/admin_dashboard');
 const feedbackHandler = require('../handlers/admin_feedback');
+const medicineHandler = require('../handlers/admin_medicine');
 
 //ADMIN ORDER
 router.get("/orders", handler.getOrders);
@@ -40,5 +41,9 @@ router.get("/totalClientsCount", dashboardHandler.getTotalClients);
 
 //ADMIN ORDER FEEDBACK
 router.get("/orderFeedback/:id", feedbackHandler.orderFeedback);
+
+//ADMIN MEDICINE 
+router.post("/addMedicine", medicineHandler.addMedicine);
+router.get("/getMedicine", medicineHandler.getMedicine);
 
 module.exports = router;
